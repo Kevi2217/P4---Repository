a37731015a0774782bd480493a94ad59058f5209
Freddy D <93995722+HunterFreddy69@users.noreply.github.com>
2023-05-09 00:00:39 +0200

    Add files via upload

--- data_extract.R
+++ data_extract.R
@@ -6,7 +6,7 @@ get_data <- function() {
                    "NDA-DK.CO", "NETC.CO", "NOVO-B.CO", "NZYM-B.CO", "ORSTED.CO",
                    "PNDORA.CO", "RBREW.CO", "ROCK-B.CO", "TRYG.CO", "VWS.CO")
   
-as.data.frame(getSymbols(paste(C25_list), src = "yahoo", auto.assign = TRUE, from = "2016-12-01", to = "2020-12-02"))
+as.data.frame(getSymbols(paste(C25_list), src = "yahoo", auto.assign = TRUE, from = "2019-12-01", to = "2020-12-02"))
   
   C25_list <- list(`AMBU-B.CO`, `BAVA.CO`, `CHR.CO`, `CARL-B.CO`, `COLO-B.CO`,
                     `DANSKE.CO`, `DEMANT.CO`, `DSV.CO`, `FLS.CO`, `GMAB.CO`,
@@ -35,3 +35,9 @@ get_data_after <- function() {
                           `PNDORA.CO`, `RBREW.CO`, `ROCK-B.CO`, `TRYG.CO`, `VWS.CO`)
   return(C25_list_after)
 }
+
+C25_names <- list("AMBU", "BAVA", "CHR", "CARL-B", "COLO-B",
+                  "DANSKE", "DEMANT", "DSV", "FLS", "GMAB",
+                  "GN", "MAERSK-A", "MAERSK-B", "ISS", "JYSK",
+                  "NDA", "NETC", "NOVO-B", "NZYM-B", "ORSTED",
+                  "PNDORA", "RBREW", "ROCK-B", "TRYG", "VWS")
--- projekt_main.R
+++ projekt_main.R
@@ -2,6 +2,7 @@ library(quantmod)
 library(lubridate)
 library(dplyr)
 library(MASS)
+library(ggplot2)
 
 source("data_extract.R")
 
@@ -10,21 +11,24 @@ source("data_extract.R")
 # 2. Find return og variance for hver stock for hele perioden baseret på disse tal. CHECK
 # 3. Udregn covariance-matrix ud fra dette. CHECK
 # 4. Udregn MVP portfolio weights.CHECK
-# 5. Eventuelt rens portfolio for meget små weights og rund op/ned (Mindst 1%). CHECK
+# 5. Eventuelt rens portfolio for meget små weights og rund op/ned (Mindst 2% før skalering). CHECK
 # 6. Bruge denne disse weights til at udregne expected return for MVP portfolio og sammenlign
 #    med den virkelige verdens OMXC25 index return for samme periode (1 år). CHECK
 
 # UPDATED PLAN
 # 1. Lav så datoerne automatisk assignes så første dag i måneden assignes for hver måned, for hele perioden. CHECK
-# 2. Udregn MVP's expected return og variance.
-# 3. Lav CML-linje.
-# 4. Lav SML-linje.
+# 2. Udregn MVP's expected return og variance. CHECK
+# 3. Lav SML-linje.
+#   DU ER NÅET TIL AT UDREGNE BETA. DU HAR PORTFØLJENS MÅNEDLIGE RETURN.
+# 4. Lav CML-linje.
 # 5. Kig på Correlation Coefficient Matrix og se på applications med distributions.
 
-C25_list <- get_data()
+
+### EXTRACTING DATA ############################################################
+C25_list_data <- get_data()
 
 # Første trading-dag af hver måned.
-ftdotm_dates <- data.frame(Date = index(C25_list[[1]]), C25_list[[1]]) %>%
+ftdotm_dates <- data.frame(Date = index(C25_list_data[[1]]), C25_list_data[[1]]) %>%
   dplyr::select(Date) %>%
   dplyr::group_by(year(Date), month(Date)) %>%
   dplyr::filter(row_number() == 1) %>%
@@ -32,17 +36,17 @@ ftdotm_dates <- data.frame(Date = index(C25_list[[1]]), C25_list[[1]]) %>%
   dplyr::select(Date)
 
 
-C25_list_after <- get_data_after()
+C25_list_after_data <- get_data_after()
 
 # Første trading-dag af hver måned AFTER.
-ftdotm_dates_after <- data.frame(Date = index(C25_list_after[[1]]), C25_list_after[[1]]) %>%
+ftdotm_dates_after <- data.frame(Date = index(C25_list_after_data[[1]]), C25_list_after_data[[1]]) %>%
   dplyr::select(Date) %>%
   dplyr::group_by(year(Date), month(Date)) %>%
   dplyr::filter(row_number() == 1) %>%
   dplyr::ungroup() %>%
   dplyr::select(Date)
 
-
+### CALCULATING EXPECTED RETURN AND VARIANCE FOR LIST OF STOCKS##########################################
 # Creating Lists and matrices.
 ex_return <- list()
 ex_return_squared <- list()
@@ -50,9 +54,13 @@ var_list <- list()
 cov_matrix <- matrix(nrow = 25, ncol = 25)
 one_vector <- c(rep(1, 25))
 C25_after_return_list <- list()
+C25_list <- list()
+monthly_market_return <- numeric(nrow(ftdotm_dates) - 1)
+C25_list_adjust <- list()
+betas <- list()
 
-for (i in seq_along(C25_list)) {
-  C25_list[[i]] <- data.frame(Date = index(C25_list[[i]]), C25_list[[i]]) %>%
+for (i in seq_along(C25_list_data)) {
+  C25_list[[i]] <- data.frame(Date = index(C25_list_data[[i]]), C25_list_data[[i]]) %>%
     dplyr::filter(Date %in% ftdotm_dates$Date) %>%
     dplyr::select(Date, 5) %>%
     dplyr::mutate(Decimal_return = (.[, 2] - lag(.[, 2]))/lag(.[, 2])) %>%
@@ -62,35 +70,91 @@ for (i in seq_along(C25_list)) {
     dplyr::mutate(div_ex_return = Decimal_return * (1 / nrow(ftdotm_dates))) %>%
     na.omit() %>%
     dplyr::summarize(ex_return = sum(div_ex_return), na.rm = TRUE) %>%
-    pull(ex_return)
+    dplyr::pull(ex_return)
   
   ex_return_squared[[i]] <-  data.frame(C25_list[[i]]) %>%
     dplyr::mutate(div_ex_return = Decimal_return^2 * 1 / nrow(ftdotm_dates)) %>%
     na.omit() %>%
     dplyr::summarize(ex_return = sum(div_ex_return), na.rm = TRUE) %>%
-    pull(ex_return)
+    dplyr::pull(ex_return)
   
   var_list[[i]] <- ex_return_squared[[i]] - (ex_return[[i]]^2)
 }
+### Creating the SML #####################################################################
+# We define the Sparinvest C25 Index weights
+real_pf_weights <- list(0.0091, 0.0071, 0.0271, 0.051, 0.0637, 
+                        0.0469, 0.0111, 0.1514, 0.0065, 0.085, 
+                        0.0097, 0.0217, 0.0289, 0.0106, 0.0105, 
+                        0.0113, 0.0059, 0.1662, 0.0355, 0.0622,
+                        0.0298, 0.0124, 0.0078, 0.0261, 0.1025) %>%
+  unlist()
+# 1. First need to calculate the the expected return of the market pf
+market_pf_ex_return <- sum(mapply(`*`, real_pf_weights, ex_return))
 
-# Filling cov-matrix entries ###################################################
+# 2. Now we need to find beta, which means that be have to find the monhtly return of the market pf (real_pf_weights)
+
+for (i in seq_len(nrow(ftdotm_dates) - 1)) {
+  total_sum <- 0
+  for (j in seq_along(C25_list_data)) {
+    C25_list_adjust[[j]] <- na.omit(C25_list[[j]])
+    total_sum <- total_sum + (C25_list_adjust[[j]][i, 3] * real_pf_weights[j])
+  }
+  monthly_market_return[i] <- total_sum
+}
+
+
+# 3. We now find beta values
+# We assume the risk free rate
+risk_free_rate <- 0.02
+
+for (i in seq_along(C25_list_data)) {
+  # Extract the beta coefficient from the regression output
+  betas[i] <- cov(na.omit(C25_list[[i]][, 3]), monthly_market_return) / var(monthly_market_return)
+}
+
+# 4. Plotting the SML model
+sml_data <- data.frame(betas = unlist(betas), expected_returns = unlist(ex_return))
+sml_data$expected_returns <- risk_free_rate + sml_data$betas * (market_pf_ex_return - risk_free_rate)
+
+colors <- factor(rainbow(25))
+names(colors) <- C25_names
+ggplot(sml_data, aes(x = betas, y = expected_returns, color = unlist(C25_names), shape = unlist(C25_names))) + 
+  geom_point() + 
+  geom_abline(intercept = risk_free_rate, slope = market_pf_ex_return - risk_free_rate) + 
+  xlab("Beta") + ylab("Expected Return") + 
+  ggtitle("Security Market Line") +
+  scale_color_manual(values = colors) + # use different colors for each asset
+  scale_shape_manual(values = rep(c(15, 16, 17, 18, 3), 5)) + # use different shapes for each asset
+  guides(color = guide_legend(title = "Assets"), shape = guide_legend(title = "Assets"))
+
+
+### CALCULATING MVP WEIGHTS AND MVP EXPECTED RETURN/VARIANCE ################################################
+# Filling cov-matrix entries 
 for (i in seq_along(C25_list)) {
   for (j in seq_along(C25_list)) {
       cov_matrix[i, j] <- 1/12 * sum(na.omit(C25_list[[i]])[, 3] * na.omit(C25_list[[j]])[, 3]) - ex_return[[i]]*ex_return[[j]]
 }
 }
-# Calculating MVP weights ######################################################
+# Calculating MVP weights
 MVP_weights <- (ginv(cov_matrix) %*% one_vector) / diag(t(one_vector) %*% ginv(cov_matrix) %*% one_vector)
-MVP_weights[MVP_weights < 0.01] <- 0
+MVP_weights[MVP_weights < 0.02] <- 0
 MVP_weights <- MVP_weights / sum(MVP_weights)
-MVP_weights <- as.list(MVP_weights[, 1])
+MVP_weights <- as.list(MVP_weights[, 1]) %>%
+  unlist()
+
+
+# Calculating the expected return and variance of the MVP
+MVP_ex_return <- sum(mapply(`*`, MVP_weights, ex_return))
+MVP_var <- t(MVP_weights) %*% cov_matrix %*% MVP_weights %>%
+  `[`(1, 1) %>% 
+  as.numeric()
+MVP_sd <- sqrt(MVP_var)
 
 
 
-
-# COMPARE - Comparing MVP with real weights of index. ############
-for (i in seq_along(C25_list_after)) {
-  C25_after_return_list[[i]] <- data.frame(Date = index(C25_list_after[[i]]), C25_list_after[[i]]) %>%
+### COMPARING MVP TO REAL DATA IN THE FOLLOWING YEAR ##################################
+for (i in seq_along(C25_list_after_data)) {
+  C25_after_return_list[[i]] <- data.frame(Date = index(C25_list_after_data[[i]]), C25_list_after_data[[i]]) %>%
     dplyr::filter(Date %in% ftdotm_dates_after$Date) %>%
     dplyr::select(Date, 5) %>%
     dplyr::slice(c(1, n())) %>%
@@ -99,15 +163,11 @@ for (i in seq_along(C25_list_after)) {
 }
 
 
-real_portfolio_weights <- list(0.0091, 0.0071, 0.0271, 0.051, 0.0637, 
-                               0.0469, 0.0111, 0.1514, 0.0065, 0.085, 
-                               0.0097, 0.0217, 0.0289, 0.0106, 0.0105, 
-                               0.0113, 0.0059, 0.1662, 0.0355, 0.0622,
-                               0.0298, 0.0124, 0.0078, 0.0261, 0.1025)
+# Calculating the return of the MVP and the real pf for the following period.
+actual_pf_return_after <- sum(mapply(`*`, real_pf_weights, C25_after_return_list))
 
-real_portfolio_return_after <- sum(mapply(`*`, real_portfolio_weights, C25_after_return_list))
+actual_MVP_return_after <- sum(mapply(`*`, MVP_weights, C25_after_return_list))
 
-MVP_return_after <- sum(mapply(`*`, MVP_weights, C25_after_return_list))
 
 
 
